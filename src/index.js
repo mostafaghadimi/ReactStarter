fde56512aebc605dc8b540b74aa8d0c3482c587b
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2019-09-26 12:53:09 +0330

    persian fonts, normalizer and etc. added. The structure of file modified :)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import * as serviceWorker from './serviceWorker';
+import './static/css/normalizer.css'
+
+export default class App extends Component {
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" render={() => (
+                    <div>
+                        سلام
+                    </div>
+                )}/>
+            </Switch>
+        )
+    }
+}
+
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    , document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
